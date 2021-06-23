dabff0d0c3442096b0acbc3f863c201044bfb697
finetuneanon <82650881+finetuneanon@users.noreply.github.com>
2021-06-23 19:32:11 +0200

    Add arguments for additive repetition penalty

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -552,6 +552,8 @@ class GenerationMixin:
         repetition_penalty: float,
         repetition_penalty_range: int,
         repetition_penalty_slope: float,
+        repetition_penalty_frequency: float,
+        repetition_penalty_presence: float,
         no_repeat_ngram_size: int,
         encoder_no_repeat_ngram_size: int,
         encoder_input_ids: torch.LongTensor,
@@ -606,8 +608,8 @@ class GenerationMixin:
                     diversity_penalty=diversity_penalty, num_beams=num_beams, num_beam_groups=num_beam_groups
                 )
             )
-        if repetition_penalty is not None and repetition_penalty != 1.0:
-            processors.append(RepetitionPenaltyLogitsProcessor(penalty=repetition_penalty, m=repetition_penalty_slope, penalize_last=repetition_penalty_range))
+        if (repetition_penalty is not None and repetition_penalty > 1.0) or (repetition_penalty_frequency is not None and repetition_penalty_frequency > 0.0) or (repetition_penalty_presence is not None and repetition_penalty_presence > 0.0)
+            processors.append(RepetitionPenaltyLogitsProcessor(penalty=repetition_penalty, m=repetition_penalty_slope, penalize_last=repetition_penalty_range, alpha_frequency=repetition_penalty_frequency, alpha_presence=repetition_penalty_presence))
         if no_repeat_ngram_size is not None and no_repeat_ngram_size > 0:
             processors.append(NoRepeatNGramLogitsProcessor(no_repeat_ngram_size))
         if encoder_no_repeat_ngram_size is not None and encoder_no_repeat_ngram_size > 0:
@@ -659,6 +661,8 @@ class GenerationMixin:
         repetition_penalty: Optional[float] = None,
         repetition_penalty_range: Optional[int] = None,
         repetition_penalty_slope: Optional[float] = 3.33,
+        repetition_penalty_frequency: Optional[float] = None,
+        repetition_penalty_presence: Optional[float] = None,
         bad_words_ids: Optional[Iterable[int]] = None,
         bos_token_id: Optional[int] = None,
         pad_token_id: Optional[int] = None,
@@ -955,6 +959,8 @@ class GenerationMixin:
             repetition_penalty=repetition_penalty,
             repetition_penalty_range=repetition_penalty_range,
             repetition_penalty_slope=repetition_penalty_slope,
+            repetition_penalty_frequency=repetition_penalty_frequency,
+            repetition_penalty_presence=repetition_penalty_presence,
             no_repeat_ngram_size=no_repeat_ngram_size,
             encoder_no_repeat_ngram_size=encoder_no_repeat_ngram_size,
             encoder_input_ids=encoder_input_ids,
